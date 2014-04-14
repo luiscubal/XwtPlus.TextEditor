6a72eef98d6a1a9cf967acfd03a3cf69d699ad07
Luís Reis <luiscubal@gmail.com>
2014-04-15 00:41:12 +0100

    Added test project.

---
+++ XwtPlus.TextEditor.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xwt;
+
+namespace XwtPlus.TextEditor.Test
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Application.Initialize(ToolkitType.Wpf);
+
+			using (var window = new MainWindow())
+			{
+				window.Show();
+				Application.Run();
+			}
+
+			Application.Dispose();
+		}
+	}
+}
